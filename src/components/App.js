c26edf630a8ee79d1e7c9a522e8605383fa7008a
Nikokay Georgiev <ngeorgiev89@outlook.com>
2021-01-23 14:14:01 +0200

    ready for deploy

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+import SearchBar from "./SearchBar";
+import VideoList from "../components/VideoList";
+import VideoDetail from "../components/VideoDetail";
+import useVideos from "../hooks/useVideos";
+
+const App = () => {
+  const [selectedVideo, setSelectedVideo] = useState(null);
+
+  const [videos, search] = useVideos("girl like me");
+
+  useEffect(() => {
+    setSelectedVideo(videos[0]);
+  }, [videos]);
+
+  return (
+    <div className="ui container">
+      <SearchBar onFormSubmit={search} />
+      <VideoDetail video={selectedVideo} />
+      <VideoList onVideoSelect={setSelectedVideo} videos={videos} />
+    </div>
+  );
+};
+
+export default App;
